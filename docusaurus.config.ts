54d45e11639e8c39cd7edbab747d6caa4df6348c
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2024-10-24 18:54:02 +0530

    copyright section updated

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -106,7 +106,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Shield, Built with ❤️ by MKSingh.dev`,
+      copyright: `Copyright © ${new Date().getFullYear()} Shield, Built with ❤️ by <a href="https://mksingh.dev" target="_blank">MKSingh.dev</a>`,
     },
     prism: {
       theme: prismThemes.github,
