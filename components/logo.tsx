294c8e06299c670daee37f205249c7e8b8a1c7b5
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2024-10-27 18:17:26 +0530

    light mode logo fixed

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
 
 const Logo = (): React.ReactNode => (
-  <div className="w-full py-1 flex gap-4 justify-center">
-    <div className="relative h-16 w-40 flex items-center justify-center">
-      <Image src="/logo.svg" alt="shield logo" fill />
+  <div className="w-full flex gap-2 justify-center">
+    <div className="relative h-12 w-40 flex items-center justify-center">
+      <Image
+        src="/logo.svg"
+        alt="shield logo"
+        fill
+        className="hidden dark:block"
+      />
+      <Image
+        src="/logo-light.svg"
+        alt="shield logo"
+        fill
+        className="block dark:hidden"
+      />
     </div>
     <pre className="pt-2">Docs</pre>
   </div>
